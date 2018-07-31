f0510b07e040e5c31020489fb56f6a7da2da114d
Ahmed Abdulkareem Rezik <ahmedrezq100@hotmail.com>
2018-07-31 11:09:34 +0300

    Incorporate StoryBoard

--- clickCounter/ViewController.swift
+++ clickCounter/ViewController.swift
@@ -14,56 +14,29 @@ class ViewController: UIViewController {
     
     
     var count = 0
-    var label: UILabel!
+   @IBOutlet var label: UILabel!
     
     
-    @objc func incrementCount(){
+    @IBAction func incrementCount(){
         self.count += 1
         self.label.text = "\(self.count)"
     }
     
-    @objc func decrementcount(){
+    @IBAction func decrementcount(){
         self.count -= 1
         self.label.text = "\(self.count)"
     }
     
-    @objc func toggleBackground(){
+    @IBAction func toggleBackground(){
       self.view.backgroundColor = UIColor.init(red: CGFloat(drand48()), green: CGFloat(drand48()), blue: CGFloat(drand48()), alpha: 1.0)
         
     }
     
     
     override func viewDidLoad() {
-        //Label
-        let label = UILabel()
-        label.frame = CGRect(x: 150, y: 250, width: 60, height: 60)
+        
         label.text = "0"
-        view.addSubview(label)
-        self.label = label
-        
-        //button
-        let button = UIButton()
-        button.frame = CGRect(x: 250, y: 150, width: 100, height: 60)
-        button.setTitle("Click Here", for: .normal)
-        button.setTitleColor(UIColor.green, for: .normal)
-        view.addSubview(button)
-        
-        let button2 = UIButton()
-        button2.frame = CGRect(x: 150, y: 150, width: 100, height: 60)
-        button2.setTitle("Click Here", for: .normal)
-        button2.setTitleColor(UIColor.red, for: .normal)
-        view.addSubview(button2)
-        
-        
-        
-        
-        button.addTarget(self, action: #selector(ViewController.incrementCount), for: UIControlEvents.touchUpInside)
-        button.addTarget(self, action: #selector(ViewController.toggleBackground), for: UIControlEvents.touchUpInside)
-         button2.addTarget(self, action: #selector(ViewController.toggleBackground), for: UIControlEvents.touchUpInside)
-       
-        button2.addTarget(self, action: #selector(ViewController.decrementcount), for: UIControlEvents.touchUpInside)
-        
-        super.viewDidLoad()
+     super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
